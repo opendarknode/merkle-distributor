cb08f0b5932cc475d9a70e7e0b39e1d43d15d34a
David Perkins <davidperkins@Davids-MacBook-Pro.local>
2021-02-28 23:53:00 -0600

    test with 5000 accounts on hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,9 +3,19 @@ import "@nomiclabs/hardhat-waffle";
 import "hardhat-typechain";
 
 const config: HardhatUserConfig = {
+    networks: {
+        hardhat: {
+            accounts: {
+                count: 5000,
+            },
+        },
+    },
     solidity: "0.7.3",
     typechain: {
         outDir: "types",
+    },
+    mocha: {
+        timeout: 50000,
     },
 };
 
